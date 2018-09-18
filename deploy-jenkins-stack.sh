b91a8bc682ef6df899e0aa5035d37b93b180e88e
Boran Wang <wangboranoliver@gmail.com>
2018-09-17 21:52:56 -0400

    Update deploy-jenkins-stack.sh

--- deploy-jenkins-stack.sh
+++ deploy-jenkins-stack.sh
@@ -2,13 +2,13 @@
 
 DATE=$(date -I)
 
-docker build . --no-cache -t jamessmith52963/jenkins:$DATE
-docker tag jamessmith52963/jenkins:$DATE jamessmith52963/jenkins:lts 
-docker tag jamessmith52963/jenkins:$DATE jamessmith52963/jenkins:latest
+docker build . --no-cache -t b96wang/jenkins:$DATE
+docker tag b96wang/jenkins:$DATE b96wang/jenkins:lts 
+docker tag b96wang/jenkins:$DATE b96wang/jenkins:latest
 
-docker push jamessmith52963/jenkins:$DATE
-docker push jamessmith52963/jenkins:latest
-docker push jamessmith52963/jenkins:lts
+docker push b96wang/jenkins:$DATE
+docker push b96wang/jenkins:latest
+docker push b96wang/jenkins:lts
 
 docker stack rm jenkins
 sleep 15
